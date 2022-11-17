8e69d5b1c5d4bf70569ceabe8c20573805c147f0
Charles <cbsal.tec@gmail.com>
2022-11-17 08:59:58 -0300

    feat(start-project): create Orders card component

---
+++ src/components/Orders/index.tsx
@@ -0,0 +1,55 @@
+import { OrderProps } from '../../types/Orders';
+import { OrdersBoard } from '../OrdersBoard';
+import { Container } from './styles';
+
+const orders: OrderProps[] = [
+  {
+    '_id': '6372e48cbcd195b0d3d0f7f3',
+    'table': '123',
+    'status': 'WAITING',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imagePath': '1668533345580-quatro-queijos.png',
+          'price': 40,
+        },
+        'quantity': 3,
+        '_id': '6372e48cbcd195b0d3d0f7f4'
+      },
+      {
+        'product': {
+          'name': 'Coca cola',
+          'imagePath': '1668533801604-coca-cola.png',
+          'price': 7,
+        },
+        'quantity': 2,
+        '_id': '6372e48cbcd195b0d3d0f7f5'
+      }
+    ],
+  }
+];
+
+export function Orders() {
+  return (
+    <Container>
+      <OrdersBoard
+        icon="🕑"
+        title="Em espera"
+        orders={orders}
+      />
+
+      <OrdersBoard
+        icon="👨‍🍳"
+        title="Em preparação"
+        orders={[]}
+      />
+
+      <OrdersBoard
+        icon="✅"
+        title="Pronto!"
+        orders={[]}
+      />
+    </Container>
+  );
+}
